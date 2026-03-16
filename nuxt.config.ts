8b1da9e3d504d080e292d061e377efe0789c990e
Ryan Boog <ryan@happydog.digital>
2026-03-15 20:13:52 -0500

    add nuxt content

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,12 +2,16 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
+
   alias: {
     '@game': new URL('./game', import.meta.url).pathname,
   },
+
   nitro: {
     experimental: {
       websocket: true,
     },
   },
+
+  modules: ['@nuxt/content'],
 })
